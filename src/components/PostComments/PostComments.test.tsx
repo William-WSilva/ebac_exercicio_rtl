bbeb0dd738c937e5f7c08bee2ef146fb7ee04df9
William Silva <williamreis.wsilva@gmail.com>
2023-08-08 17:32:30 -0300

    Testando comentarios

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -5,15 +5,18 @@ describe('Teste para o componente PostComment', () => {
   test('Deve renderizar o comentário: Bem legal', () => {
     render(<PostComment children={undefined} imageUrl={''} />);
 
-    const bemLegalCommentElement = screen.getByText('Bem legal');
-    expect(bemLegalCommentElement).toBeInTheDocument();
+    const ComentarioA = screen.getByText('Bem legal');
+    expect(ComentarioA).toBeInTheDocument();
   });
-
   test('Deve renderizar o comentário: O veiculo ficou muito top', () => {
     render(<PostComment children={undefined} imageUrl={''} />);
 
-    const ficouTopCommentElement = screen.getByText('O veiculo ficou muito top');
-    expect(ficouTopCommentElement).toBeInTheDocument();
+    const ComentarioB = screen.getByText('O veiculo ficou muito top');
+    expect(ComentarioB).toBeInTheDocument();
   });
 });
+
+
+
+
 
