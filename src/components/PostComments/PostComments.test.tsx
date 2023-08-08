35f492496c47e0bcb8697264e35a2815d8cd8400
William Silva <williamreis.wsilva@gmail.com>
2023-08-08 16:10:56 -0300

    Configurando teste para os 2 comentarios

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -4,13 +4,11 @@ import PostComment from '.';
 describe('Teste para o componente PostComment', () => {
   it('Deve renderizar o comentário "Bem legal"', () => {
     render(<PostComment />);
-    
     expect(screen.getByText("Bem legal")).toBeInTheDocument();
   });
 
   it('Deve renderizar o comentário "O veiculo ficou muito top"', () => {
     render(<PostComment />);
-    
     expect(screen.getByText("O veiculo ficou muito top")).toBeInTheDocument();
   });
 });
